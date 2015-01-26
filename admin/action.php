fe8837c1d2543424a00362f5833c30b370cd15d3
Goragod Wiriya <admin@goragod.com>
2015-01-26 18:05:44 +0700

    mhlv[

---
+++ admin/action.php
@@ -0,0 +1,55 @@
+<?php
+	// admin/action.php
+	header("content-type: text/html; charset=UTF-8");
+	// inint
+	include '../bin/inint.php';
+	// ตรวจสอบ referer และ admin
+	if (gcms::isReferer() && gcms::isAdmin()) {
+		// action
+		$action = gcms::getVars($_POST, 'action', '');
+		if ($action == 'clearcache') {
+			// เคลียร์แคช
+			$cache = new gcmsCache(DATA_PATH.'cache/');
+			$errors = $cache->clear();
+			if (is_array($errors)) {
+				$ret = array('error' => 'SOME_FILE_NOT_DELETE');
+			} elseif ($errors) {
+				$ret = array('error' => 'CLEAR_CACHE_COMPLETE');
+			}
+		} elseif (isset($_SESSION['login']['account']) && $_SESSION['login']['account'] == 'demo') {
+			$ret = array('error' => 'EX_MODE_ERROR');
+		} else {
+			if (preg_match('/(deletemail)_([0-9]+)/', $_POST['data'], $match)) {
+				$email = $db->getRec(DB_EMAIL_TEMPLATE, $match[2]);
+				if ($email && $email['email_id'] == 0) {
+					$db->delete(DB_EMAIL_TEMPLATE, $email['id']);
+					// มีการติดตั้ง mailmerge ลบจดหมายที่ส่งแล้ว
+					if (defined('DB_MAILMERGE')) {
+						$db->query("DELETE FROM `".DB_MAILMERGE."` WHERE `email_id`= '$email[id]'");
+					}
+					// คืนค่า
+					$ret = array('error' => 'DELETE_SUCCESS', 'remove' => "M_{$email[id]}");
+				} else {
+					$ret = array('error' => 'ACTION_ERROR');
+				}
+			} elseif ($action == 'zone' || ($action == 'delete' && $_POST['module'] == 'country')) {
+				// ตรวจสอบ id
+				$ids = array();
+				foreach (explode(',', $_POST['id']) AS $id) {
+					$ids[] = (int)$id;
+				}
+				$ids = implode(',', $ids);
+				if ($ids != '') {
+					if ($action == 'zone') {
+						$db->query("UPDATE `".DB_COUNTRY."` SET `zone`=".(int)$_POST['value']." WHERE `id` IN($ids)");
+					} else {
+						$db->query("DELETE FROM `".DB_COUNTRY."` WHERE `id` IN($ids)");
+					}
+				}
+			}
+		}
+	} else {
+		$ret = array('error' => 'ACTION_ERROR');
+	}
+	// คืนค่าเป็น JSON
+	echo gcms::array2json($ret);
