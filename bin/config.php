98b3fdf615a5b6cb2c282d3e32380dfeb2b2f5d3
Goragod Wiriya <admin@goragod.com>
2015-01-29 18:49:49 +0700

    Merge origin/master
    Conflicts:
    	bin/config.php

--- bin/config.php
+++ bin/config.php
@@ -1,13 +1,12 @@
 <?php
 	// bin/config.php
+	$config['db_username'] = 'root';
+	$config['web_title'] = 'GCMS';
 	$config['hour'] = 0;
+	$config['languages']['0'] = 'th';
+	$config['skin'] = 'default';
 	$config['ftp_host'] = '';
 	$config['ftp_username'] = '';
 	$config['ftp_password'] = '';
 	$config['ftp_root'] = '';
 	$config['ftp_port'] = 21;
-	$config['languages']['0'] = 'th';
-	$config['skin'] = 'default';
-	$config['db_username'] = '';
-	$config['web_title'] = 'GCMS';
-	
